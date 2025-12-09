e04cbf83188328ffb5dc0113491a631cbf0d593c
Zehan <zehan.kareem@gmail.com>
2025-12-10 02:49:43 +0600

    init(iamzehan): Added mobile menu

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,26 +1,38 @@
-import { Link } from "react-router-dom";
+import NavLinks from "./nav-links";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
+import clsx from 'clsx';
 export default function Navigation() {
-  const pages = [
-    {
-      name: "Home",
-      link: "/",
-    },
-    {
-      name: "Shop",
-      link: "/shop",
-    },
-    {
-      name: "Cart",
-      link: "/cart",
-    },
-  ];
+  const [show, setMenu] = useState(false);
+  const handleMenu = () => {
+    setMenu(!show);
+  };
   return (
-    <ul className="w-full h-10 bg-orange-500 flex items-center justify-end">
-      <li className="flex h-full *:flex *:items-center *:transition-colors *:duration-300">
-        {pages.map((page) => (
-          <Link to={page.link} className="text-xl px-4 h-full hover:bg-orange-300">{page.name}</Link>
-        ))}
+    <ul className="w-full h-8 md:h-10 lg:bg-orange-500 flex items-center justify-end lg:border border-orange-500">
+      {/* PC Navs */}
+      <li className="hidden lg:flex h-full *:flex *:items-center *:transition-colors *:duration-300">
+        <NavLinks />
+      </li>
+      {/* Mobile Navs */}
+      <li className="lg:hidden flex gap-2 w-full h-full *:flex *:items-center *:transition-colors *:duration-300">
+        <div className="flex flex-col w-full">
+          <span onClick={handleMenu} className="h-full w-full flex justify-end active:*:bg-gray-200/10 *:transition-all *:duration-300">
+            <Icon show={show} />
+          </span>
+          <div className={clsx({"z-500 flex flex-col h-screen w-screen text-center rounded-b border-3 border-orange-500 bg-orange-500 transition-all duration-300 origin-top opacity-100 translate-y-0":show}, {"-translate-y-full opacity-0":!show})}>
+            <NavLinks />
+          </div>
+        </div>
       </li>
     </ul>
   );
+}
+
+function Icon({ show }: { show: boolean }) {
+  if (show) {
+    return <CloseIcon fontSize="large" className="rounded"/>;
+  } else {
+    return <MenuIcon fontSize="large" className="rounded"/>;
+  }
 }
---
+++ src/components/Navigation/nav-links.tsx
@@ -0,0 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+import clsx from 'clsx';
+
+export default function NavLinks() {
+  const location = useLocation();
+  const pages = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Shop",
+      link: "/shop",
+    },
+    {
+      name: "Cart",
+      link: "/cart",
+    },
+  ];
+  return (
+    <>
+      {pages.map((page) => (
+        <Link
+          to={page.link}
+          className={clsx("text-xl px-4 h-full md:rounded-none hover:bg-orange-300",
+            {"bg-orange-300": location.pathname === page.link},
+          )}
+        >
+          {page.name}
+        </Link>
+      ))}
+    </>
+  );
+}
